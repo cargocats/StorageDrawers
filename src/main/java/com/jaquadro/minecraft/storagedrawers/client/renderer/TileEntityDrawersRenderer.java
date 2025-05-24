e4fe9683e582df74eb4234ea3e4c45d34df51474
cargocats <ceggy@protonmail.com>
2025-05-24 12:29:23 -0400

    Change how depth works (again) to how it renders the items

--- src/main/java/com/jaquadro/minecraft/storagedrawers/client/renderer/TileEntityDrawersRenderer.java
+++ src/main/java/com/jaquadro/minecraft/storagedrawers/client/renderer/TileEntityDrawersRenderer.java
@@ -410,7 +410,7 @@ public class TileEntityDrawersRenderer extends TileEntitySpecialRenderer {
                 side,
                 offsetX,
                 offsetY,
-                block.halfDepth ? depth : depth - 1.0f,
+                1f - depth + block.getTrimDepth() - 0.005f,
                 alpha);
     }
 
